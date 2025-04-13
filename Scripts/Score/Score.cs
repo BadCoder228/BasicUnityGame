8bc84eb7debc5f36153c638e70bfe677f799f3f9
Wormy <167223943+BadCoder228@users.noreply.github.com>
2025-04-13 09:32:03 +0700

    Delete Scripts directory

--- Scripts/Score/Score.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEngine;
-
-public class Score : MonoBehaviour
-{
-    public int CharacterScore, EnemyScore = 0;
-}
